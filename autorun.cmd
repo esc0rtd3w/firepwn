86f3707afc5d44240743c337a31fe2e5040d5d2a
Jason Borowski <esc0rtd3w@gmail.com>
2017-10-31 18:53:45 -0400

    Added Initial Files

---
+++ autorun.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+:reset
+
+title FirePWN   [Amazon FireStick/FireTV Super Script]           [esc0rtd3w]
+
+mode con lines=46
+
+color 0e
+
+
+:: Start Main Script
+call "%~dp0firepwn.cmd"
+
+
+:end
+
